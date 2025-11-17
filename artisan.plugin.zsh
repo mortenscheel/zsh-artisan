ee6ccb399d3446f55d11b33e18c9b32370cdee6d
Morten Scheel <morten@mortenscheel.com>
2025-11-17 11:44:36 +0100

    don't detect docker-compose

--- artisan.plugin.zsh
+++ artisan.plugin.zsh
@@ -17,23 +17,8 @@ function artisan() {
     local laravel_path=`dirname $artisan_path`
     local docker_compose_config_path=`find $laravel_path -maxdepth 1 \( -name "docker-compose.yml" -o -name "docker-compose.yaml" \) | head -n1`
     local artisan_cmd
+    artisan_cmd="php $artisan_path"
 
-    if [ "$docker_compose_config_path" = '' ]; then
-        artisan_cmd="php $artisan_path"
-    else
-        if [ "`grep "laravel/sail" $docker_compose_config_path | head -n1`" != '' ]; then
-            artisan_cmd="$laravel_path/vendor/bin/sail artisan"
-        else
-            local docker_compose_cmd=`_docker_compose_cmd`
-            local docker_compose_service_name=`$docker_compose_cmd ps --services 2>/dev/null | grep 'app\|php\|api\|workspace\|laravel\.test\|webhost' | head -n1`
-            if [ -t 1 ]; then
-                artisan_cmd="$docker_compose_cmd exec $docker_compose_service_name php artisan"
-            else
-                # The command is not being run in a TTY (e.g. it's being called by the completion handler below)
-                artisan_cmd="$docker_compose_cmd exec -T $docker_compose_service_name php artisan"
-            fi
-        fi
-    fi
 
     local artisan_start_time=`date +%s`
 
